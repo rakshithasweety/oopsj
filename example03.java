fccab76865f7f69460d833802cf767c5605f0787
rakshithasweety <rakshithasweety2007@gmail.com>
2026-08-31 19:41:38 +0530

    Add files via upload

---
+++ example03.java
@@ -0,0 +1,12 @@
+public class java1 {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b); // Addition
+        System.out.println(a - b); // Subtraction
+        System.out.println(a * b); // Multiplication
+        System.out.println(a / b); // Division
+        System.out.println(a % b); // Modulus (remainder)
+    }
+}
---
+++ example2.java
@@ -0,0 +1,11 @@
+public class java2 {
+    public static void main(String args[]) {
+
+
+        System.out.println(10<<2); // Addition
+        System.out.println(10<<3); // Subtraction
+        System.out.println(20<<2); // Multiplication
+        System.out.println(15<<4); // Division
+
+    }
+}
